9bca304213fe44eca6bf8f3f996d9edfd113db24
Богдан <sysoevbogdan82@gmail.com>
2024-09-12 18:35:27 +0300

    feat: added all icons and images. Changed tailwind, eslint and prettier config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="wrapper"></div>
+  <div class="wrapper">
+    <h1 class="text-5xl">Hello world</h1>
+  </div>
 </template>
 
 <style scoped></style>
