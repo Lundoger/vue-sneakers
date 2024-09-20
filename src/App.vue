9d6b70ce2280d994591ae6ecaaad8bc337ee2926
Богдан <sysoevbogdan82@gmail.com>
2024-09-20 22:07:02 +0300

    feat: finished card and added basic markup for list

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import HeaderComponent from './components/HeaderComponent.vue'
+import SneakersList from './components/sneakers-list/SneakersList.vue'
 </script>
 
 <template>
   <div class="wrapper">
     <HeaderComponent />
+    <main class="py-12">
+      <SneakersList />
+    </main>
   </div>
 </template>
 
---
+++ src/components/sneakers-list/SneakersList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import SneakerCard from './SneakerCard.vue'
+</script>
+
+<template>
+  <section>
+    <div class="container">
+      <h2 class="mb-10 text-4xl font-bold">Все кроссовки</h2>
+      <ul class="grid grid-cols-5 gap-6">
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+        <SneakerCard />
+      </ul>
+    </div>
+  </section>
+</template>
