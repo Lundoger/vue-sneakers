0f63327c5a53205095bd88312696260ef73d7c94
Богдан <sysoevbogdan82@gmail.com>
2024-09-22 21:31:04 +0300

    feat: finished sneaker card and created cart modal

---
+++ src/components/SneakersList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import SneakerCard from './SneakerCard.vue'
+
+const onCLickAdd = () => {
+  alert(111)
+}
+const onCLickFavorite = () => {
+  alert(222)
+}
+</script>
+
+<template>
+  <ul class="grid grid-cols-5 gap-6">
+    <SneakerCard
+      image-url="/sneakers/sneakers-11.jpg"
+      title="Krossivki top"
+      price="9999 uah"
+      :is-added="false"
+      :is-liked="true"
+      v-on:click-add="onCLickAdd"
+      v-on:click-favorite="onCLickFavorite"
+    />
+  </ul>
+</template>
